c4d44def788684cd68be86c9aacebc7b37e3cfc7
Yesquero <yesquero1986@gmail.com>
2022-03-20 05:08:12 +0300

    Updated README.md.

--- src/main/java/ru/mirea/cookbook/RecipeBookApplication.java
+++ src/main/java/ru/mirea/cookbook/RecipeBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CookbookApplication {
+public class RecipeBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CookbookApplication.class, args);
+		SpringApplication.run(RecipeBookApplication.class, args);
 	}
 
 }
