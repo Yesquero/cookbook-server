9ca6a6c056cc7a995137664042e4429822a64a0d
Yesquero <yesquero1986@gmail.com>
2022-03-22 20:02:53 +0300

    Profile set for tests.

--- src/test/java/ru/mirea/recipebook/CookbookApplicationTests.java
+++ src/test/java/ru/mirea/recipebook/CookbookApplicationTests.java
@@ -3,7 +3,9 @@ package ru.mirea.recipebook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = "test")
 @SpringBootTest
 class CookbookApplicationTests {
 
