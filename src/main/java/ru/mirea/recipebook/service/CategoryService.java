79023ddf21c5c26127e035950b81224895575067
Yesquero <yesquero1986@gmail.com>
2022-05-19 21:30:42 +0300

    More endpoints.

--- src/main/java/ru/mirea/recipebook/service/CategoryService.java
+++ src/main/java/ru/mirea/recipebook/service/CategoryService.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.mirea.recipebook.domain.RecipeCategory;
 import ru.mirea.recipebook.repository.CategoryRepository;
+import ru.mirea.recipebook.utility.DomainLogicException;
+import ru.mirea.recipebook.utility.ResourceConflictException;
 import ru.mirea.recipebook.utility.ResourceNotFoundException;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -19,8 +24,33 @@ public class CategoryService {
 			.orElseThrow(() -> new ResourceNotFoundException(RecipeCategory.class, name));
 	}
 
+	@Transactional
+	public RecipeCategory addCategory(String name) {
+		Optional<RecipeCategory> foundCategory = categoryRepository.findByName(name);
+		if (foundCategory.isPresent()) {
+			throw new ResourceConflictException(RecipeCategory.class, name);
+		}
+
+		RecipeCategory newCategory = new RecipeCategory();
+		newCategory.setName(name);
+
+		return categoryRepository.saveAndFlush(newCategory);
+	}
+
 	public List<RecipeCategory> getAll() {
 		return categoryRepository.findAll();
+	}
+
+	@Transactional
+	public void delete(UUID uuid) {
+		RecipeCategory toDelete = categoryRepository.findById(uuid)
+			.orElseThrow(() -> new ResourceNotFoundException(RecipeCategory.class, uuid));
+
+		if (!toDelete.getRecipes().isEmpty()) {
+			throw new DomainLogicException("Cannot delete category with linked recipes.");
+		}
+
+		categoryRepository.delete(toDelete);
 	}
 
 }
