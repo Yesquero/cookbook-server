5345c3a943168c55b2c2c1d3adc8440c21958bc1
Yesquero <69271414+Yesquero@users.noreply.github.com>
2022-05-19 21:32:59 +0300

    Feature/endpoints (#11)
    * Domain entities setup
    
    * Domain entities setup
    
    * Domain entities setup
    
    * Implemented basic image upload, download endpoints
    
    * Implemented basic image upload, download endpoints
    
    * Simplified domain model.
    
    * Implemented basic auth.
    
    * Implemented basic auth.
    
    * Added endpoints for recipes, categories, users.
    
    * Added endpoints for recipes, categories, users.
    
    * Revert "Added endpoints for recipes, categories, users."
    
    This reverts commit 2c775148820b856004cb4c3304ddbe302eba75c0.
    
    * Added duration in hours and minutes.
    
    * Merged conflict, updated schema.
    
    * More endpoints.

--- src/main/java/ru/mirea/recipebook/controller/CategoryController.java
+++ src/main/java/ru/mirea/recipebook/controller/CategoryController.java
@@ -1,15 +1,15 @@
 package ru.mirea.recipebook.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import ru.mirea.recipebook.controller.dto.RecipeCategoryDto;
 import ru.mirea.recipebook.controller.dto.mapper.DtoMapper;
 import ru.mirea.recipebook.domain.RecipeCategory;
 import ru.mirea.recipebook.service.CategoryService;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,11 +20,23 @@ public class CategoryController {
 	private final CategoryService categoryService;
 	private final DtoMapper mapper;
 
+	@PostMapping("/add/{name}")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
+	public RecipeCategoryDto addCategory(@PathVariable String name) {
+		return mapper.toDto(categoryService.addCategory(name));
+	}
+
 	@GetMapping("/all")
 	public List<RecipeCategoryDto> getAll() {
 		List<RecipeCategory> categories = categoryService.getAll();
 
 		return categories.stream().map(mapper::toDto).collect(Collectors.toList());
+	}
+
+	@DeleteMapping("/delete/{uuid}")
+	@PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
+	public void delete(@PathVariable UUID uuid) {
+		categoryService.delete(uuid);
 	}
 
 }
